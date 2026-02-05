58b8d9ffa6e2bf8a52884a398c9118efe7f90bb5
Roger Duran <rogermao5.1@gmail.com>
2026-02-05 00:10:30 -0500

    Seguridad en hilos

--- Laboratorio1/reto3/reto3.java
+++ Laboratorio1/reto3/reto3.java
@@ -1,4 +1,20 @@
+import java.util.*;
+import java.util.function.Function;
+
 public class reto3 {
-    
-    
+    public static String ecoFinal(String mensaje) {
+        return new StringBuilder(mensaje).reverse().toString();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingresa el mensaje: ");
+        String entrada = sc.nextLine();
+
+        Function<String, String> eco = msg -> ecoFinal(msg);
+
+        System.out.println(eco.apply(entrada));
+    }
 }
