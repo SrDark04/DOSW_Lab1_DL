1ed2ec1108a08f860adcbab5f4254f21b4e50673
Roger Duran <rogermao5.1@gmail.com>
2026-02-05 12:32:06 -0500

    Parte B, Roger

--- Laboratorio1/reto6/reto6.java
+++ Laboratorio1/reto6/reto6.java
@@ -1,4 +1,32 @@
-import java.util.*;
 public class reto6 {
+    public static void ejecutarComando(String comando) {
 
+        switch (comando) {
+            case "BROMEAR":
+                System.out.println("La máquina ríe: ¿Por qué la RAM rompió con la CPU? Porque necesitaba espacio…");
+                break;
+
+            case "GRITAR":
+                System.out.println("La máquina grita: ¡¡¡ALERTA DE STACK OVERFLOW!!!");
+                break;
+
+            case "SUSURRAR":
+                System.out.println("La máquina susurra: Shhh… los bugs están dormidos");
+                break;
+
+            case "ANALIZAR":
+                System.out.println("La máquina procesa: Analizando datos… resultado: ¡Eres increíble programando!");
+                break;
+
+            default:
+                System.out.println("Comando no reconocido.");
+        }
+    }
+
+    public static void main(String[] args) {
+        ejecutarComando("BROMEAR");
+        ejecutarComando("GRITAR");
+        ejecutarComando("SUSURRAR");
+        ejecutarComando("ANALIZAR");
+    }
 }
