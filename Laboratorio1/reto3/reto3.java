8ae39aaf9f3845875592b7d2de3f06450f0946d9
Camilo Leon <camilo.leon-a@mail.escuelaing.edu.co>
2026-02-05 00:02:42 -0500

    primer lamnda

--- Laboratorio1/reto3/reto3.java
+++ Laboratorio1/reto3/reto3.java
@@ -1,4 +1,19 @@
-public class reto3 {
-    
-    
-}
+public static String ecoFinal(String mensaje) {
+        String repetido = IntStream.range(0, 3)
+                .mapToObj(i -> mensaje)
+                .collect(Collectors.joining(" "));
+
+        return repetido;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Ingresa el mensaje: ");
+        String entrada = sc.nextLine();
+
+        Function<String, String> eco = msg -> ecoFinal(msg);
+
+        System.out.println(eco.apply(entrada));
+    }
